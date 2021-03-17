de392d7766e2096e73c395d0a3cb1000355593dd
MarcosVP-Fatec <marcos.pereira47@fatec.sp.gov.br>
2021-03-17 22:32:32 +0000

    Autorização - Parei no teste com erro

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.gov.sp.fatec.springbootapp.entity.Autorizacao;
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
+import br.gov.sp.fatec.springbootapp.repository.AutorizacaoRepository;
 import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 
 @SpringBootTest
@@ -17,13 +19,14 @@ class SpringBootAppApplicationTests {
 
     @Autowired 
     private UsuarioRepository usuarioRepo;
+    private AutorizacaoRepository autorizacaoRepo;
 
 	@Test
 	void contextLoads() {
     }
     
     @Test
-    void testeTbInserelUsuario(){
+    void testeTblUsuarioInsere(){
 
         Usuario usuario = new Usuario();
         usuario.setNome("Teste X");
@@ -34,7 +37,7 @@ class SpringBootAppApplicationTests {
     }
 
     @Test
-    void testeTblExcluiUsuario(){
+    void testeTblUsuarioExclui(){
 
         Long local_id;
         Usuario usuario = new Usuario();
@@ -45,6 +48,31 @@ class SpringBootAppApplicationTests {
         usuarioRepo.delete(usuario);
         assertFalse(usuarioRepo.existsById(local_id));
         
+    }
+
+    @Test
+    void testeTbAutorizacaoInsere(){
+
+        Autorizacao autorizacao = new Autorizacao();
+        autorizacao.setNome("REGRA_DE__TESTE");
+        autorizacaoRepo.save(autorizacao);
+/*
+        System.out.println("##########################################");
+        System.out.println("##########################################");
+        System.out.println(autorizacao.getId());
+        System.out.println(autorizacao.getNome());
+        System.out.println("##########################################");
+        System.out.println("##########################################");
+        //assertNotNull(autorizacao.getId());
+        assertNotNull(autorizacaoRepo.findById(autorizacao.getId()));*/
+    }
+
+    @Test
+    void testeTbUsuarioJoinAutorizacao(){
+
+        Usuario usuario = usuarioRepo.findById(1L).get();
+        assertEquals("ROLE_ADMIN", usuario.getAutorizacoes().iterator().next().getNome());
+
     }
 
 }
