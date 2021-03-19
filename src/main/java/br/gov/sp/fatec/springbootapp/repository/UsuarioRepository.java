fde4618de4baa24af7840481c4235a642ce5c642
MarcosVP-Fatec <marcos.pereira47@fatec.sp.gov.br>
2021-03-19 19:32:46 +0000

    Testes com JPQL

--- src/main/java/br/gov/sp/fatec/springbootapp/repository/UsuarioRepository.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package br.gov.sp.fatec.springbootapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
 
@@ -9,4 +12,39 @@ import br.gov.sp.fatec.springbootapp.entity.Usuario;
    */
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     
+    /**
+     * I Spring fornece um Query Method   
+     * Meu resultado pode voltar uma lista ou somente um
+     * O nome do método tem que começar com "findBy" + atributo
+     * Ex. para o atributo "nome"
+     *  findByNome (Que seria o mesmo que findByNomeEquals)
+     * Ex. para usar o like
+     *  findByNomeContains(String nome);
+     *  findByNomeContainsIgnoreCase(String nome);
+     * 
+     * 
+     */
+    public List<Usuario> findByNomeContainsIgnoreCase(String nome);
+
+    public Usuario findByNome(String nome); //O atributo nome é unique key.
+
+    @Query("select u from Usuario u where u.nome = ?1")
+    public Usuario buscarUsuarioPorNome(String nome);
+
+    //Para buscar por mais de um parâmetro (Exemplo para login)
+    public Usuario findByNomeAndSenha(String nome, String senha);
+
+    @Query("select u from Usuario u where u.senha = ?2 and u.nome = ?1")
+    public Usuario buscarUsuarioPorNomeESenha(String nome, String senha);
+
+    //Buscar todo mundo que é administrador
+    //O Nome, neste caso, é um atributo da classe Autorizacao
+    public List<Usuario> findByAutorizacoesNome(String nomeAutorizacao);
+
+    //Join
+    @Query("select u from Usuario u inner join u.autorizacoes a where a.nome = ?1 order by u.nome")
+    public List<Usuario> buscarUsuariosPorNomeDeAutorizacao(String nomeAutorizacao);
+
+
+
 }
--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashSet;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,24 +26,71 @@ class SpringBootAppApplicationTests {
     @Autowired 
     private AutorizacaoRepository autorizacaoRepo;
 
-	@Test
+    /**
+     * Função que cria um usuário para evitar reescrita em todas as classes de teste
+     */
+    private Usuario criaUsuarioTestolino(){
+        Usuario usuario = new Usuario();
+        usuario.setNome("Testolino");
+        usuario.setSenha("senha123");
+        return usuarioRepo.save(usuario);
+    }
+
+    /**
+     * Função que cria um usuário para evitar reescrita em todas as classes de teste
+     */
+    private Usuario criaUsuarioTestolinoComAutorizacao( Autorizacao autorizacao ){
+        Usuario usuario = new Usuario();
+        usuario.setNome("Testolino");
+        usuario.setSenha("senha123");
+        usuario.setAutorizacoes(new HashSet<Autorizacao>());
+        usuario.getAutorizacoes().add(autorizacao);
+        return usuarioRepo.save(usuario);
+    }
+
+    /**
+     * Função que cria uma regra de autorização para evitar reescrita em todas as classes de teste
+     */
+    private Autorizacao criaAutorizacaoTeste(){
+        Autorizacao autorizacao = new Autorizacao();
+        autorizacao.setNome("#REGRA_TESTE");
+        return autorizacaoRepo.save(autorizacao);
+    }
+
+    private void testeQuebra(String titulo){
+        System.out.println("######################################################################################");
+        System.out.println("##############################    " + titulo);
+        System.out.println("######################################################################################");
+        System.out.println("##################################################################################");
+        System.out.println("##############################################################################");
+        System.out.println("##########################################################################");
+        System.out.println("######################################################################");
+        System.out.println("##################################################################");
+        System.out.println("##############################################################");
+        System.out.println("##########################################################");
+        System.out.println("######################################################");
+        System.out.println("##################################################");
+        System.out.println("##############################################");
+        System.out.println("##########################################");
+    }
+
+    @Test
 	void contextLoads() {
     }
     
     @Test
     void testeTblUsuarioInsere(){
-
+        testeQuebra("USUARIO - Insere");
         Usuario usuario = new Usuario();
         usuario.setNome("Teste X");
         usuario.setSenha("$enhaF0rte");
         usuarioRepo.save(usuario);
         assertNotNull(usuario.getId());
-        
     }
 
     @Test
     void testeTblUsuarioExclui(){
-
+        testeQuebra("USUARIO - Exclui");
         Long local_id;
         Usuario usuario = new Usuario();
         usuario.setNome("Teste X");
@@ -51,12 +99,11 @@ class SpringBootAppApplicationTests {
         local_id = usuario.getId();
         usuarioRepo.delete(usuario);
         assertFalse(usuarioRepo.existsById(local_id));
-        
     }
 
     @Test
     void testeTblAutorizacaoInsere(){
-
+        testeQuebra("AUTORIZACAO - Insere");
         Autorizacao autorizacao = new Autorizacao();
         autorizacao.setNome("REGRA_DE__TESTE");
         autorizacaoRepo.save(autorizacao);
@@ -66,7 +113,7 @@ class SpringBootAppApplicationTests {
 
     @Test
     void testeTblUsuarioComAutorizacaoInsere(){
-
+        testeQuebra("USUARIO com AUTORIZACAO - Insere");
         Autorizacao autorizacao = new Autorizacao();
         autorizacao.setNome("REGRA_TESTE_1");
         autorizacaoRepo.save(autorizacao);
@@ -80,7 +127,65 @@ class SpringBootAppApplicationTests {
         usuarioRepo.save(usuario);
 
         assertEquals("Mitces", usuarioRepo.getOne(usuario.getId()).getNome() );
+    }
 
+    @Test
+    void testePesqUsuarioNomeContains(){
+        testeQuebra("PESQUISA USUARIO - Contém Nome");
+        this.criaUsuarioTestolino();
+        List<Usuario> usuarios = usuarioRepo.findByNomeContainsIgnoreCase("ino");
+        assertFalse(usuarios.isEmpty());
+    }
+
+    @Test
+    void testePesqUsuarioPorNomeExato(){
+        testeQuebra("PESQUISA USUARIO - Nome exato");
+        this.criaUsuarioTestolino();
+        assertNotNull(usuarioRepo.findByNome("Testolino"));
+    }
+
+    void testePesqUsuarioPorNomeExatoQuery(){
+        testeQuebra("PESQUISA USUARIO - Nome exato Query");
+        this.criaUsuarioTestolino();
+        assertNotNull(usuarioRepo.buscarUsuarioPorNome("Testolino"));
+    }
+/*
+    @Test
+    void testBuscarUsuarioPorNome(){
+        testeQuebra("BUSCAR USUARIO - Por Nome");
+        this.criaUsuarioTestolino();
+        Usuario usuario = usuarioRepo.buscarUsuarioPorNome("Testolino");
+        assertEquals(usuario.getSenha(), "senha123");
+    }
+*/
+    @Test
+    void testePesqUsuarioNomeAutorizacao(){
+        testeQuebra("PESQUISA USUARIO - Nome da Autorização");
+        this.criaUsuarioTestolinoComAutorizacao(this.criaAutorizacaoTeste());
+        List<Usuario> usuarios = usuarioRepo.findByAutorizacoesNome("#REGRA_TESTE");
+        assertEquals(1, usuarios.size());
+    }
+
+    @Test
+    void testePesqUsuarioNomeSenha(){
+        testeQuebra("PESQUISA USUARIO - Nome e Senha");
+        this.criaUsuarioTestolino();
+        assertNotNull(usuarioRepo.findByNomeAndSenha("Testolino", "senha123"));
+    }
+
+    @Test
+    void testePesqUsuarioNomeSenhaQuery(){
+        testeQuebra("PESQUISA USUARIO - Nome e Senha - Query");
+        this.criaUsuarioTestolino();
+        assertNotNull(usuarioRepo.buscarUsuarioPorNomeESenha("Testolino", "senha123"));
+    }
+
+    @Test
+    void testBuscarUsuariosPorNomeDeAutorizacao(){
+        testeQuebra("BUSCAR USUARIO - Por Nome de Autorização");
+        this.criaUsuarioTestolinoComAutorizacao(this.criaAutorizacaoTeste());
+        List<Usuario> usuarios = usuarioRepo.buscarUsuariosPorNomeDeAutorizacao("#REGRA_TESTE");
+        assertTrue(usuarios.size() == 1);
     }
 
 }
