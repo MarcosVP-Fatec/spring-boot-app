cbb6a5059e08caa7ad1c9b7cdb6378f325c40965
MarcosVP-Fatec <marcos.pereira47@fatec.sp.gov.br>
2021-05-14 23:39:54 +0000

    Técnicas JUnit

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -29,90 +29,89 @@ class SpringBootAppApplicationTests {
     @Autowired
     private SegurancaService segurancaService;
 
-    /**
-     * Função que cria um usuário para evitar reescrita em todas as classes de teste
-     */
-    private Usuario criaUsuarioTestolino(){
-        Usuario usuario = new Usuario();
-        usuario.setNome("Testolino");
-        usuario.setSenha("senha123");
-        return usuarioRepo.save(usuario);
+    @BeforeAll
+    static void init(@Autowired JdbcTemplate jdbcTemplate){
+        
+        jdbcTemplate.update(
+            "insert into usr_usuario (usr_nome, usr_senha) values(?,?)", "Mineda", "SenhaF0rte");
+
+        jdbcTemplate.update(
+            "insert into aut_autorizacao (aut_nome) values(?)", "ROLE_ADMIN");
+
+        jdbcTemplate.update(
+            "insert into uau_usuario_autorizacao (usr_id, aut_id) values(?,?)", 1L, 1L);
+
+        jdbcTemplate.update(
+            "insert into usr_usuario (usr_nome, usr_senha) values(?,?)", "UsuEXCLUIR", "Senha123");
+
+        jdbcTemplate.update(
+            "insert into aut_autorizacao (aut_nome) values(?)", "ROLE_NAO_EXCLUIR");
+
+        jdbcTemplate.update(
+            "insert into uau_usuario_autorizacao (usr_id, aut_id) values(?,?)", 2L, 2L);
+
     }
 
-    /**
-     * Função que cria um usuário para evitar reescrita em todas as classes de teste
-     */
-    private Usuario criaUsuarioTestolinoComAutorizacao( Autorizacao autorizacao ){
-        Usuario usuario = new Usuario();
-        usuario.setNome("Testolino");
-        usuario.setSenha("senha123");
-        usuario.setAutorizacoes(new HashSet<Autorizacao>());
-        usuario.getAutorizacoes().add(autorizacao);
-        return usuarioRepo.save(usuario);
-    }
-
-    /**
-     * Função que cria uma regra de autorização para evitar reescrita em todas as classes de teste
-     */
-    private Autorizacao criaAutorizacaoTeste(){
-        Autorizacao autorizacao = new Autorizacao();
-        autorizacao.setNome("#REGRA_TESTE");
-        return autorizacaoRepo.save(autorizacao);
-    }
-
-    private void testeQuebra(String titulo){
+    private static void testeQuebra(String titulo){
+        System.out.println("######################################################################################");
+        System.out.println("######################################################################################");
         System.out.println("######################################################################################");
         System.out.println("##############################    " + titulo);
         System.out.println("######################################################################################");
         System.out.println("##################################################################################");
         System.out.println("##############################################################################");
         System.out.println("##########################################################################");
-        System.out.println("######################################################################");
-        System.out.println("##################################################################");
-        System.out.println("##############################################################");
-        System.out.println("##########################################################");
-        System.out.println("######################################################");
-        System.out.println("##################################################");
-        System.out.println("##############################################");
-        System.out.println("##########################################");
     }
 
     @Test
-	void contextLoads() {
+    void existeUsuario(){
+        testeQuebra("EXISTE USUÁRIO");
+        assertNotNull(usuarioRepo.buscarUsuarioPorNome("Mineda"));
     }
-    
+    @Test
+    void naoExisteUsuario(){
+        testeQuebra("NÃO EXISTE USUÁRIO");
+        assertNull(usuarioRepo.buscarUsuarioPorNome("Moneda"));
+    }
+
     @Test
     void testeTblUsuarioInsere(){
-        testeQuebra("USUARIO - Insere");
+        testeQuebra("USUARIO - Insere Repository");
         Usuario usuario = new Usuario();
-        usuario.setNome("Teste X");
+        usuario.setNome("UsuarioInsere");
         usuario.setSenha("$enhaF0rte");
-        usuarioRepo.save(usuario);
+        usuarioRepo.saveAndFlush(usuario);
         assertNotNull(usuario.getId());
+    }
+
+    @Test
+    void testarServicoCriaUsuario(){
+        testeQuebra("USUARIO - Insere Service");
+        Usuario usuario = segurancaService.criarUsuario("UsuInsereService", "senha123", "#REGRA_NORMAL");
+        assertNotNull(usuario);
+    }
+
+    @Test
+    void testBuscarUsuariosPorNomeDeAutorizacao(){
+        testeQuebra("BUSCAR USUARIO - Por Nome de Autorização");
+        List<Usuario> usuarios = usuarioRepo.buscarUsuariosPorNomeDeAutorizacao("ROLE_ADMIN");
+        assertTrue(usuarios.size() != 0);
     }
 
     @Test
     void testeTblUsuarioExclui(){
         testeQuebra("USUARIO - Exclui");
         Long local_id;
-        Usuario usuario = new Usuario();
-        usuario.setNome("Teste X");
-        usuario.setSenha("$enhaF0rte");
-        usuarioRepo.save(usuario);
+        Usuario usuario = usuarioRepo.findById(2L).get();
         local_id = usuario.getId();
         usuarioRepo.delete(usuario);
         assertFalse(usuarioRepo.existsById(local_id));
     }
 
     @Test
-    void testeTblAutorizacaoInsere(){
-
-        testeQuebra("AUTORIZACAO - Insere");
-        Autorizacao autorizacao = new Autorizacao();
-        autorizacao.setNome("REGRA_DE__TESTE");
-        autorizacaoRepo.save(autorizacao);
-        assertNotNull(autorizacao.getNome());
-        assertNotNull(autorizacao.getId());
+    void testePesqUsuarioNomeSenhaQuery(){
+        testeQuebra("PESQUISA USUARIO - Nome e Senha - Query");
+        assertTrue(usuarioRepo.buscarUsuarioPorNomeESenha("Mineda", "SenhaF0rte").getId() != null);
     }
 
     @Test
@@ -134,82 +133,75 @@ class SpringBootAppApplicationTests {
     }
 
     @Test
+    void testeTblAutorizacaoInsere(){
+        testeQuebra("AUTORIZACAO - Insere");
+        Autorizacao autorizacao = new Autorizacao();
+        autorizacao.setNome("REGRA_DE__TESTE");
+        autorizacaoRepo.save(autorizacao);
+        assertNotNull(autorizacao.getNome());
+        assertNotNull(autorizacao.getId());
+    }
+
+    @Test
     void testePesqUsuarioNomeContains(){
         testeQuebra("PESQUISA USUARIO - Contém Nome");
-        this.criaUsuarioTestolino();
-        List<Usuario> usuarios = usuarioRepo.findByNomeContainsIgnoreCase("ino");
+        List<Usuario> usuarios = usuarioRepo.findByNomeContainsIgnoreCase("ined");
         assertFalse(usuarios.isEmpty());
     }
 
     @Test
     void testePesqUsuarioPorNomeExato(){
         testeQuebra("PESQUISA USUARIO - Nome exato");
-        this.criaUsuarioTestolino();
-        assertNotNull(usuarioRepo.findByNome("Testolino"));
+        assertNotNull(usuarioRepo.findByNome("Mineda").getId());
     }
 
     void testePesqUsuarioPorNomeExatoQuery(){
         testeQuebra("PESQUISA USUARIO - Nome exato Query");
-        this.criaUsuarioTestolino();
-        assertNotNull(usuarioRepo.buscarUsuarioPorNome("Testolino"));
+        assertNotNull(usuarioRepo.buscarUsuarioPorNome("Mineda"));
     }
-/*
+
     @Test
     void testBuscarUsuarioPorNome(){
         testeQuebra("BUSCAR USUARIO - Por Nome");
-        this.criaUsuarioTestolino();
-        Usuario usuario = usuarioRepo.buscarUsuarioPorNome("Testolino");
-        assertEquals(usuario.getSenha(), "senha123");
+        Usuario usuario = usuarioRepo.buscarUsuarioPorNome("Mineda");
+        assertEquals(usuario.getSenha(), "SenhaF0rte");
     }
-*/
+
     @Test
     void testePesqUsuarioNomeAutorizacao(){
         testeQuebra("PESQUISA USUARIO - Nome da Autorização");
-        this.criaUsuarioTestolinoComAutorizacao(this.criaAutorizacaoTeste());
-        List<Usuario> usuarios = usuarioRepo.findByAutorizacoesNome("#REGRA_TESTE");
+        List<Usuario> usuarios = usuarioRepo.findByAutorizacoesNome("ROLE_ADMIN");
         assertEquals(1, usuarios.size());
     }
 
+    /*
     @Test
-    void testePesqUsuarioNomeSenha(){
-        testeQuebra("PESQUISA USUARIO - Nome e Senha");
-        this.criaUsuarioTestolino();
-        assertTrue(usuarioRepo.findByNomeAndSenha("Testolino", "senha123") != null);
+	void contextLoads() {
     }
 
-    @Test
-    void testePesqUsuarioNomeSenhaQuery(){
-        testeQuebra("PESQUISA USUARIO - Nome e Senha - Query");
-        this.criaUsuarioTestolino();
-        assertTrue(usuarioRepo.buscarUsuarioPorNomeESenha("Testolino", "senha123") != null);
+    // Função que cria um usuário para evitar reescrita em todas as classes de teste
+    private Usuario criaUsuarioTestolino(){
+        Usuario usuario = new Usuario();
+        usuario.setNome("Testolino");
+        usuario.setSenha("senha123");
+        return usuarioRepo.save(usuario);
     }
 
-    @Test
-    void testBuscarUsuariosPorNomeDeAutorizacao(){
-        testeQuebra("BUSCAR USUARIO - Por Nome de Autorização");
-        this.criaUsuarioTestolinoComAutorizacao(this.criaAutorizacaoTeste());
-        List<Usuario> usuarios = usuarioRepo.buscarUsuariosPorNomeDeAutorizacao("#REGRA_TESTE");
-        assertTrue(usuarios.size() == 1);
+    // Função que cria um usuário para evitar reescrita em todas as classes de teste
+    private Usuario criaUsuarioTestolinoComAutorizacao( Autorizacao autorizacao ){
+        Usuario usuario = new Usuario();
+        usuario.setNome("Testolino");
+        usuario.setSenha("senha123");
+        usuario.setAutorizacoes(new HashSet<Autorizacao>());
+        usuario.getAutorizacoes().add(autorizacao);
+        return usuarioRepo.save(usuario);
     }
 
-    @Test
-    void testarServicoCriaUsuario(){
-        Usuario usuario = segurancaService.criarUsuario("Pessoa#normal", "senha123", "#REGRA_NORMAL");
-        assertNotNull(usuario);
+    //Função que cria uma regra de autorização para evitar reescrita em todas as classes de teste
+    private Autorizacao criaAutorizacaoTeste(){
+        Autorizacao autorizacao = new Autorizacao();
+        autorizacao.setNome("#REGRA_TESTE");
+        return autorizacaoRepo.save(autorizacao);
     }
-
-    @BeforeAll
-    static void init(@Autowired JdbcTemplate jdbcTemplate){
-        
-        jdbcTemplate.update(
-            "insert into usr_usuario (usr_nome, usr_senha) values(?,?)", "Mineda", "SenhaF0rte");
-
-        jdbcTemplate.update(
-            "insert into aut_autorizacao (aut_nome) values(?)", "ROLE_ADMIN");
-
-        jdbcTemplate.update(
-            "insert into uau_usuario_autorizacao (usr_id, aut_id) values(?,?)", 1L, 1L);
-
-    }
-
+*/
 }
